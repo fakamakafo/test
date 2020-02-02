62f30c10622fdc074646525f4111393270e70b8b
fakamakafo <fakamakafo@gmail.com>
2020-02-02 20:01:25 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,16 +14,15 @@ module.exports = function(grunt) {
     },
 
     connect: {
+      livereload: {
+        options: {
+          open: true,
+        }
+      },
       server: {
         options: {
-          port: 8000,
-          hostname: '*',
-          onCreateServer: function(server, connect, options) {
-            // var io = require('socket.io').listen(server);
-            // io.sockets.on('connection', function(socket) {
-            //   // do something with socket
-            // });
-          }
+          port: 8080,
+          hostname: '*'
         }
       }
     },
